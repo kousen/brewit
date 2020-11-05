6bf5f0f295a9b83b499abe4b2df271514914502f
Ken Kousen <ken.kousen@kousenit.com>
2020-11-05 15:05:04 -0500

    Add yet even more examples

--- app/src/main/java/com/brewery/training/playground/main.kt
+++ app/src/main/java/com/brewery/training/playground/main.kt
@@ -7,7 +7,15 @@ fun useNullableString(arg: String?) {
 
 }
 
+val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+
+fun isOdd(x: Int) = x % 2 != 0
+
 fun main(args: Array<String>) {
+    val numbers = listOf(1, 2, 3, 4, 5)
+    println(numbers.filter(::isOdd))
+    println(numbers.filter { num -> isOdd(num) })
+
     println("Hello, ${if (args.isNotEmpty()) args[0] else "World"}!")
     println(sayHello("Dolly"))
     var personName: String? = "Fred"
