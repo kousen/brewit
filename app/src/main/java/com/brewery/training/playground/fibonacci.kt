63fb30ef604fc19db127dfac648723efe33743a4
Ken Kousen <ken.kousen@kousenit.com>
2020-11-09 10:37:50 -0500

    Modify fibonacci sequence to start at 0

--- app/src/main/java/com/brewery/training/playground/fibonacci.kt
+++ app/src/main/java/com/brewery/training/playground/fibonacci.kt
@@ -1,13 +1,12 @@
 package com.brewery.training.playground
 
-fun fibonacciFold(n: Long) =
+fun fibonacci(n: Long) =
     (2..n + 1).fold(1 to 0) { (prev, curr), _ ->
+        println(curr)
         curr to (prev + curr)
     }.second
 
-// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 fun main() {
-    println(fibonacciFold(100))
-
-    val arrayOfNullStringSearch = arrayOfNulls<String>(5)
+    println(fibonacci(10))
 }
